83562ef42d214cfeb8b937ba1c0dafad5c381eae
mbero-akoko <mberoakoko@gmail.com>
2026-01-07 20:29:12 +0100

    Added some changes

--- src/experiments/convergence_of_covariances.py
+++ src/experiments/convergence_of_covariances.py
@@ -12,6 +12,7 @@ from matplotlib.figure import Figure
 
 matplotlib.use("TkAgg")
 plt.rcParams.update({"font.size": 7})
+plt.style.use("https://github.com/dhaitz/matplotlib-stylesheets/raw/master/pitayasmoothie-dark.mplstyle")
 
 @dataclasses.dataclass
 class ConvergenceCriterion:
@@ -34,33 +35,7 @@ def discrete_lyapunov_function(f: NDArray, p: NDArray, q: NDArray) -> Iterable[N
     return values
 
 
-def __generate_covariances(num: int) -> Iterable[NDArray]:
-    pass
-
-
-def __create_covariance_trajectories(covanriances: Iterable[NDArray]) -> list[Iterable[NDArray]]:
-    ...
-
-def plot_covariance_trajectories(trajectories: list[Iterable[NDArray]]) -> None:
-    def flattened_trajectorues(_trajectories: list[Iterable[NDArray]]) -> Iterable[NDArray]:
-        for item in _trajectories:
-            item = np.array(item)
-            item.reshape(item.shape[0], -1)
-            yield item
-
-    fig: Figure = plt.figure(figsize=(16, 9))
-    ax:Axes = fig.add_subplot()
-    for i, item in enumerate(flattened_trajectorues(trajectories)):
-        for j in range(4):
-            ax.plot(item[:, j], color=f"C{i}")
-
-    plt.tight_layout()
-    plt.show()
-
-
-
-
-if __name__ == "__main__":
+def test_some_things():
     f = np.array([
         [0.2, 0.4],
         [-0.4, 1]
@@ -68,7 +43,7 @@ if __name__ == "__main__":
 
     q = np.diag([1, 2])
 
-    p = - 1* np.eye(2)
+    p = - 1 * np.eye(2)
 
     # covariance_values = iter(discrete_lyapunov_function(f, p, q))
     covariance_values = np.array(list(discrete_lyapunov_function(f, p, q)))
@@ -79,3 +54,48 @@ if __name__ == "__main__":
         plt.plot(covariance_values[:, i], color=f"C{i}")
     plt.tight_layout()
     plt.show()
+
+
+def __generate_covariances(num: int) -> Iterable[NDArray]:
+    alpha_nums = np.random.randint(-50, 50, size=num)
+    yield from (alpha * np.eye(2) for alpha in alpha_nums)
+
+
+def __create_covariance_trajectories(covariances: Iterable[NDArray]) -> list[Iterable[NDArray]]:
+    f = np.array([
+        [0.2, 0.4],
+        [-0.4, 1]
+    ])
+
+    q = np.diag([1, 2])
+
+    return list(discrete_lyapunov_function(f, p, q) for p in covariances)
+
+def __plot_covariance_trajectories(trajectories: list[Iterable[NDArray]]) -> None:
+    def flattened_trajectorues(_trajectories: list[Iterable[NDArray]]) -> Iterable[NDArray]:
+        for item in _trajectories:
+            item = np.array(list(item))
+            item = item.reshape((item.shape[0], -1))
+            yield item
+
+    fig: Figure = plt.figure(figsize=(16, 9))
+    ax:Axes = fig.add_subplot()
+    for i, item in enumerate(flattened_trajectorues(trajectories)):
+        for j in range(4):
+            ax.plot(item[:, j], color=f"C{i}")
+
+    plt.title("Covariance Trajectories")
+    plt.tight_layout()
+    plt.savefig('../../data/images/covariance_trajectories.png', bbox_inches='tight')
+    # plt.show()
+
+
+def perform_covariance_convergence_experiment():
+    cov_iter = __generate_covariances(100)
+    trajectories = __create_covariance_trajectories(cov_iter)
+    __plot_covariance_trajectories(trajectories)
+
+
+
+if __name__ == "__main__":
+    perform_covariance_convergence_experiment()
---
+++ src/experiments/readMe.md
@@ -0,0 +1,6 @@
+# Here are the results of a bunch of experiments I have run 
+Might reorder soon.
+
+### First . Convergence of Covariances
+
+![Covariance Experiment](../../data/images/covariance_trajectories.png)
