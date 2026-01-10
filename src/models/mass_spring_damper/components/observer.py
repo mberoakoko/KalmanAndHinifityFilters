ba22a77e25ca1fec9397228f539ce4fc171c777a
mbero-akoko <mberoakoko@gmail.com>
2026-01-10 22:47:31 +0100

    Observer IMplemented

--- src/models/mass_spring_damper/components/observer.py
+++ src/models/mass_spring_damper/components/observer.py
@@ -8,3 +8,28 @@ def _generate_optimal_observer_gain(system: control.StateSpace, Q: NDArray, R: N
     P, _, _ = control.lqe(system.A, system.C, Q, R)
     return P
 
+@dataclasses.dataclass
+class LuenbergerObserver:
+    system: control.StateSpace
+    Q: NDArray
+    R: NDArray
+    L: NDArray | None = dataclasses.field(init=False)
+
+    def __post_init__(self):
+        self.L = _generate_optimal_observer_gain(self.system, self.Q, self.R)
+
+    def __update(self, t: float, x: NDArray, u: NDArray , params: dict = None) -> NDArray:
+        u_, y = u
+        return self.system.A @ x + self.system.B @ u_ @ (y - self.L @ x)
+
+    def __output(self, t: float, x: NDArray, u: NDArray, params:dict = None) -> NDArray:
+        return self.system.C @ x 
+
+
+    def create_observer(self) -> control.NonlinearIOSystem:
+        return control.NonlinearIOSystem(
+            self.__update, self.__output,
+            name="Observer",
+            inputs=("", ),
+            outputs=("", ),
+        )
